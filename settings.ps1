0923b1e7862e0695a83077b86af5f646ee867b43
Tobias Studer <studert@live.com>
2019-03-11 11:45:51 +0100

    Add event feature

--- settings.ps1
+++ settings.ps1
@@ -4,8 +4,8 @@ $SitePostFix = "dev.local"
 $webroot = "C:\inetpub\wwwroot"
 $HabitatSiteName = "habitat.dev.local"
 
-$SitecoreVersion = "9.0.0 rev. 171002"
-$InstallerVersion = "1.0.2"
+$SitecoreVersion = "9.1.0 rev. 001564"
+$InstallerVersion = "2.0.0"
 
 # Certificates
 $CertPath = "$PSScriptRoot\build\certificates"
