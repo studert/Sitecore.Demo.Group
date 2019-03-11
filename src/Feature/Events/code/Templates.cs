90ecb812bf131d411e5678c4e74916044c45f22b
Tobias Studer <studert@live.com>
2019-03-11 13:24:25 +0100

    Add some templates

---
+++ src/Feature/Events/code/Templates.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Data;
+
+namespace code
+{
+  public struct Templates
+  {
+    public struct Event
+    {
+      public static readonly ID ID = new ID("{4AAA969B-BBCE-4306-92F9-C360A17F01C1}");
+      public struct Fields
+      {
+        public static readonly ID Title = new ID("{61BF64E9-9B12-40D7-94E5-EBD7E14F567B}");
+        public static readonly ID Description = new ID("{4C2113F7-28EE-47BC-972B-5435E2CFD9EF}");
+        public static readonly ID Date = new ID("{002D2BC2-22FB-4C43-A2FC-6402CCED9A3C}");
+        public static readonly ID Venue = new ID("{76262186-B8D1-4BD9-9228-6EBDA9430605}");
+        public static readonly ID VenueAddress = new ID("{066B3DD9-6EE2-4F16-B6ED-34E104BFC18D}");
+      }
+    }
+  }
+}
